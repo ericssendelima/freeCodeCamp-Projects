e9608df76dfe274d1193fee798078dc742194d55
Yalê Ericssen <78110731+ericssendelima@users.noreply.github.com>
2023-03-10 19:04:55 -0300

    Create intervalo-numeros-recursao.js

---
+++ javascript/js-basico/intervalo-numeros-recursao.js
@@ -0,0 +1,16 @@
+if (endNum < startNum) {
+    return [];
+  } else {
+    const numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+
+/*
+function rangeOfNumbers(startNum, endNum) {
+  return endNum < startNum
+    ? []
+    : [...rangeOfNumbers(startNum, endNum - 1), endNum];
+}
+
+*/
