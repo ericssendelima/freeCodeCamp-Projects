98d1183b89fb2eef895bf439bca2e4bbbcb4deb5
Yalê Ericssen <78110731+ericssendelima@users.noreply.github.com>
2023-03-10 18:58:21 -0300

    Update contagem-regressiva-recursao-duasFormas.js

--- javascript/js-basico/contagem-regressiva-recursao-duasFormas.js
+++ javascript/js-basico/contagem-regressiva-recursao-duasFormas.js
@@ -17,4 +17,8 @@ console.log(countdown(5));  //[5, 4, 3, 2, 1]
 function countdown(n){
    return n < 1 ? [] : [n, ...countdown(n - 1)];
 }
+
+
+se mudar a linha 8 para arr.push,
+o resultado será [1, 2, 3, 4, 5]
 */
