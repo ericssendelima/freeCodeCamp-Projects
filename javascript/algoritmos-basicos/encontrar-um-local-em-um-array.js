322a036c11cd0ab94a43b0cf8e57173a62d6db28
Yalê Ericssen <78110731+ericssendelima@users.noreply.github.com>
2023-04-01 12:16:09 -0300

    Create encontrar-um-local-em-um-array.js

---
+++ javascript/algoritmos-basicos/encontrar-um-local-em-um-array.js
@@ -0,0 +1,31 @@
+/*
+Retorne o menor índice em que um valor (segundo argumento) deve ser inserido no array (primeiro argumento)
+ assim que tenha sido ordenado. O valor retornado deve ser um número.
+*/
+function getIndexToIns(arr, num) {
+  let result = [];
+  arr.sort((a, b) => a - b);
+  
+  if(num > arr[arr.length - 1]){//se num for maior que o ultimo dado do array, adiciona ele em result
+    result.push(arr[arr.length - 1])    
+  }else if(arr.includes(num)){//se num for igual a algum dado em arr, retorna o indice desse dado em arr
+    result.push(arr[arr.indexOf(num)])
+    return arr.indexOf(result[0]);
+  }else{
+    result = arr.filter(function(n){
+      if(arr[arr.indexOf(n)] <= num && arr[arr.indexOf(n) + 1] >= num){    
+//se num está em um intervalo dentro de arr, retorna o menor índice em que um valor (segundo argumento) deve ser inserido no array
+        return true
+      };
+    });
+  };
+
+  return arr.indexOf(result[0]) + 1;
+};
+
+console.log(getIndexToIns([5, 3, 20, 3], 5));
+console.log(getIndexToIns([2, 20, 10], 19));
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30));
+
+
+ 
